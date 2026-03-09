74814abe6b77bba9543e7777863dc472895d461c
Mario Aichinger <aichingm@gmail.com>
2026-03-09 23:45:49 +0100

    [neovim] Add shift+o binding to open file with in default application

--- .config/nvim/package/neo-tree.lua
+++ .config/nvim/package/neo-tree.lua
@@ -19,6 +19,25 @@ return {
       },
       window = {
         width = 35,
+        mappings = {
+          ["O"] = {
+            command = function(state)
+              local filepath = state.tree:get_node().path
+              local osType = os.getenv("OS")
+              local command
+
+              if osType == "Windows_NT" then
+                command = "start " .. filepath
+              elseif osType == "Darwin" then
+                command = "open " .. filepath .. " 1>/dev/null 2>&1"
+              else
+                command = "xdg-open " .. filepath .. " 1>/dev/null 2>&1"
+              end
+              os.execute(command)
+            end,
+            desc = "open_with_system_defaults",
+          },
+        },
       },
     })
   end,
